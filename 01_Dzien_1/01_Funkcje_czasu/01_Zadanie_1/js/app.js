204782cd38c657991b28ea493b170d80e036d4ac
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-12 15:01:24 +0200

    task 1 & 2

--- 01_Dzien_1/01_Funkcje_czasu/01_Zadanie_1/js/app.js
+++ 01_Dzien_1/01_Funkcje_czasu/01_Zadanie_1/js/app.js
@@ -1,1 +1,5 @@
+let tclock = setTimeout(function () { console.log("JavaScript Rules") }, 4000);
+let clockInterval = setInterval(() => console.log("Wygenerowano z setInterval"), 10000);
+clockInterval();
+tclock();
 
--- 01_Dzien_1/01_Funkcje_czasu/02_Zadanie_2/js/app.js
+++ 01_Dzien_1/01_Funkcje_czasu/02_Zadanie_2/js/app.js
@@ -1,1 +1,14 @@
+function countHello(val) {
+	if ((val == 1 || val > 1) && val <= 10) {
+		let counter = 0;
+		let clockInt = setInterval(function h() {
+			counter++;
+			console.log(`Hello${counter}`);
+			if (counter >= val) {
+				clearInterval(clockInt);
+			}
+		}, 500);
+	}
 
+}
+countHello(8);
