488b21ed9d8b93a57754a90cea898608a62b0f3c
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-13 14:24:22 +0200

    Day1 03_Tablicy 08 done

--- 01_Dzien_1/03_Tablice/08_Zadanie_8/js/app.js
+++ 01_Dzien_1/03_Tablice/08_Zadanie_8/js/app.js
@@ -1,1 +1,15 @@
 
+function addArrays(arr1, arr2) {
+	let resArr = arr1.length >= arr2.length ? new Array(arr1.length) : new Array(arr2.length);
+	for (let i = 0; i < resArr.length; i++) {
+		if (arr1[i] != undefined && arr2[i] != undefined) {
+			resArr[i] = arr1[i] + arr2[i];
+		}
+		else {
+			resArr[i] = arr1[i] != undefined ? arr1[i] : arr2[i];
+		}
+	}
+	return resArr;
+}
+
+console.log(addArrays([4, 0, 1, 3, 4], [1, 9, 6, 7, 8, 17]));
