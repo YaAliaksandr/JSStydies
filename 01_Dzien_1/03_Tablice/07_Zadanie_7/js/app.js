e167830bc7de5979ec84d69bf94b6eebfbc686cb
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-13 14:05:21 +0200

    Day1 03_Tablicy 07 done

--- 01_Dzien_1/03_Tablice/07_Zadanie_7/js/app.js
+++ 01_Dzien_1/03_Tablice/07_Zadanie_7/js/app.js
@@ -1,1 +1,11 @@
 
+let someArr = [145, 11, 3, 64, 4, 6, 10];
+
+function sortArray(arr) {
+	return arr.sort((a, b) => {
+		if (a > b) { return 1; }
+		else if (a == b) { return 0; }
+		else { return -1; }
+	});
+}
+console.log(sortArray(someArr));
