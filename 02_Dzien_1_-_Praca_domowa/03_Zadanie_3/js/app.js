b5cb52c0fb7a7ffee65f8cc592138b64e0970eef
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-13 15:50:09 +0200

    02_Dzien_1_-_Praca_domowa 03 done

--- 02_Dzien_1_-_Praca_domowa/03_Zadanie_3/js/app.js
+++ 02_Dzien_1_-_Praca_domowa/03_Zadanie_3/js/app.js
@@ -1,34 +1,48 @@
 const planets = [
-  {
-    name: "Merkury",
-    numberOfMoons: 0
-  },
-  {
-    name: "Wenus",
-    numberOfMoons: 0
-  },
-  {
-    name: "Ziemia",
-    numberOfMoons: 1
-  },
-  {
-    name: "Mars",
-    numberOfMoons: 2
-  },
-  {
-    name: "Jowisz",
-    numberOfMoons: 63
-  },
-  {
-    name: "Saturn",
-    numberOfMoons: 60
-  },
-  {
-    name: "Uran",
-    numberOfMoons: 27
-  },
-  {
-    name: "Neptun",
-    numberOfMoons: 13
-  }
+	{
+		name: "Merkury",
+		numberOfMoons: 0
+	},
+	{
+		name: "Wenus",
+		numberOfMoons: 0
+	},
+	{
+		name: "Ziemia",
+		numberOfMoons: 1
+	},
+	{
+		name: "Mars",
+		numberOfMoons: 2
+	},
+	{
+		name: "Jowisz",
+		numberOfMoons: 63
+	},
+	{
+		name: "Saturn",
+		numberOfMoons: 60
+	},
+	{
+		name: "Uran",
+		numberOfMoons: 27
+	},
+	{
+		name: "Neptun",
+		numberOfMoons: 13
+	}
 ];
+
+let resArr = planets.sort(function (a, b) {
+	if (a.numberOfMoons < b.numberOfMoons) {
+		return 1;
+	}
+	else if (a.numberOfMoons == b.numberOfMoons) {
+		return 0;
+	}
+	else {
+		return -1;
+	}
+});
+
+console.log(resArr);
