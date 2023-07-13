f07dfc58c99c26bd86806591a21c05aa1a03b17a
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-13 13:50:54 +0200

    Day1 03_Tablicy 05 done

--- 01_Dzien_1/03_Tablice/06_Zadanie_6/js/app.js
+++ 01_Dzien_1/03_Tablice/06_Zadanie_6/js/app.js
@@ -1,1 +1,28 @@
 
+function getEvenAverage(arr) {
+	if (Array.isArray(arr) && arr.length > 0) {
+		let counter = 0;
+		let arrReduce = arr.reduce(function (accum, item) {
+			if (item % 2 === 0) {
+				accum += item;
+				counter++
+			}
+			return accum;
+		}, 0);
+		if (counter > 0) {
+			let res = arrReduce / counter;
+			if (res % 1 !== 0) {
+				res = res.toFixed(3);
+			}
+			return res;
+		}
+		else {
+			return null;
+		}
+	}
+}
+console.log(getEvenAverage([1, 2, 3, 4, 5, 6, 7]));
+console.log(getEvenAverage([1, 1, 1, 1, 1, 1]));
+console.log(getEvenAverage([2, 8, 3, 7, 4]));
+
+
