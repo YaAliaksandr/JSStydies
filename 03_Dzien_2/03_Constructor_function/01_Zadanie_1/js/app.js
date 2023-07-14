326a5ddac3305d99126b315629dd66c9ef939538
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-14 12:51:30 +0200

    03_Dzien_2 03_Constructor_function 01_Zadanie_1

---
+++ 03_Dzien_2/03_Constructor_function/01_Zadanie_1/js/app.js
@@ -0,0 +1,35 @@
+
+function Basket() {
+	this.products = [];
+	this.sum = 0;
+};
+
+Basket.prototype.addProduct = function (name, price, quantity) {
+	this.products.push({ "nazwa towaru": name, "cena": price, "ilosc": quantity });
+}
+Basket.prototype.showBasket = function () {
+	let str = `Twoje zamówienie : `;
+	let userSumm = 0;
+	this.products.forEach(element => {
+		let productName = element["nazwa towaru"];
+		let productPrice = element["cena"];
+		let productQuantity = element["ilosc"];
+		let productSumm = element["cena"] * parseFloat(element["ilosc"]);
+		let productSummZl = parseInt(productSumm);
+		let productSummGrStr = `${Math.round(productSumm * 100) / 100}`.slice(-2);
+		str += `
+		${productName} cena ${productPrice} w iłosci ${productQuantity} summa do zapłaty ${productSummZl} zl. ${productSummGrStr} gr.`;
+		userSumm += Math.round(parseFloat(`${productSummZl}.${productSummGrStr}`) * 100) / 100;
+	});
+	this.sum = userSumm.toFixed(2);
+	return str;
+};
+
+
+
+let user1 = new Basket();
+user1.addProduct("apple", 40, "1.5344 kg");
+user1.addProduct("watermellon", 12, 1);
+console.log(user1);
+console.log(user1.showBasket());
+console.log(user1.sum);
