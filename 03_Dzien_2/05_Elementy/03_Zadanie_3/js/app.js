b205b6b58722bd2675cfcf0a97031434771372a0
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-15 13:56:24 +0200

    03_Dzien_2 05_Elementy 03_Zadanie_3

--- 03_Dzien_2/05_Elementy/03_Zadanie_3/js/app.js
+++ 03_Dzien_2/05_Elementy/03_Zadanie_3/js/app.js
@@ -1,1 +1,9 @@
 const blocks = document.querySelectorAll(".block");
+for (let val of blocks) {
+	console.log(val.innerText);
+}
+// console.log(blocks);
+for (let val of blocks) {
+	val.innerText = "Nowa wartość diva o klasie blocks"
+	console.log(val.innerText);
+}
