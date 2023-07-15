5649d890521e6cd9116c072d49fbd63eec3f2408
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-15 14:13:26 +0200

    03_Dzien_2 05_Elementy 05_Zadanie_5

--- 03_Dzien_2/05_Elementy/05_Zadanie_5/js/app.js
+++ 03_Dzien_2/05_Elementy/05_Zadanie_5/js/app.js
@@ -1,1 +1,10 @@
 const childElements = document.querySelector(".offers").querySelectorAll("div, h2, p");
+console.log(childElements);
+function getTags(elements) {
+	let elArr = [];
+	for (let val of elements) {
+		elArr.push(val.tagName);
+	}
+	return elArr;
+}
+console.log(getTags(childElements));
