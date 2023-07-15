8a8294ec7a8e883e15e8693962bbe131097ab8b0
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-15 14:24:45 +0200

    03_Dzien_2 05_Elementy 06_Zadanie_6

--- 03_Dzien_2/05_Elementy/06_Zadanie_6/js/app.js
+++ 03_Dzien_2/05_Elementy/06_Zadanie_6/js/app.js
@@ -1,1 +1,9 @@
 const banner = document.querySelector(".ban");
+console.dir(banner);
+function getClassInfo(element) {
+
+	let className = element.className;
+	let classArr = className.split(' ');
+	return classArr;
+}
+console.log(getClassInfo(banner));
