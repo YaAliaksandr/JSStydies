47d59e5e74c2f779951ea601035286362e005c74
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-15 12:11:07 +0200

    03_Dzien_2 05_Elementy 02_Zadanie_2

--- 03_Dzien_2/04_Wyszukiwanie_elementow/02_Zadanie_2/js/app.js
+++ 03_Dzien_2/04_Wyszukiwanie_elementow/02_Zadanie_2/js/app.js
@@ -1,1 +1,5 @@
-
+console.log(document.querySelector("#home"));
+console.log(home);
+console.log(document.getElementById("home"));
+console.log(document.querySelector("li[data-direction]"));
+console.log(document.querySelector(".block"));
