24ecd39f57a59901c3c259cdd565876457b396ff
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-15 13:50:43 +0200

    03_Dzien_2 05_Elementy 01_Zadanie_1 02_Zadanie_2

--- 03_Dzien_2/05_Elementy/01_Zadanie_1/js/app.js
+++ 03_Dzien_2/05_Elementy/01_Zadanie_1/js/app.js
@@ -1,1 +1,12 @@
 const links = document.querySelector(".links").querySelectorAll("li");
+function getDataInfo(elements) {
+	let arr = [];
+	for (let val of elements) {
+		// console.log(val);
+		if (val.hasAttribute("data-color")) {
+			arr.push(val.getAttribute("data-color"))
+		}
+	}
+	return arr;
+}
+console.log(getDataInfo(links));
--- 03_Dzien_2/05_Elementy/02_Zadanie_2/js/app.js
+++ 03_Dzien_2/05_Elementy/02_Zadanie_2/js/app.js
@@ -3,3 +3,28 @@ const children = document.querySelector(".offers").querySelectorAll("div, h2, p"
 const ban = document.querySelector(".ban");
 const someBlocks = document.querySelectorAll(".block");
 const listElements = document.querySelector(".links").querySelectorAll("li");
+console.log(children);
+function check(...args) {
+	for (let i = 0; i < args.length; i++) {
+
+		if (args[i].length > 0) {
+			let str = `Zmienna № ${i + 1} jest pseudoTablicą i zaviera:`;
+
+			for (let el of args[i]) {
+				let strPlus;
+				if (el.className !== "") {
+					strPlus += `
+					tagName = ${el.tagName} with class ${el.className}`
+				}
+				else {
+					strPlus += `
+					tagName = ${el.tagName} without class`
+				}
+				str += strPlus
+			}
+			console.log(str);
+		}
+	}
+}
+
+check(home, children, ban, someBlocks, listElements);
