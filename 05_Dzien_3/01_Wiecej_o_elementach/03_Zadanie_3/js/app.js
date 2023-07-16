3fd36e7801c8b735c1508d41c7da450d4622314e
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-16 16:16:04 +0200

    05_Dzien_3 01_Wiecej_o_elementach 03_Zadanie_3

--- 05_Dzien_3/01_Wiecej_o_elementach/03_Zadanie_3/js/app.js
+++ 05_Dzien_3/01_Wiecej_o_elementach/03_Zadanie_3/js/app.js
@@ -1,3 +1,9 @@
 /*
   Poniżej napisz kod rozwiązujący zadania
  */
+let myName = document.querySelector("#name");
+myName.innerText = "Yakubouski Aliaksandr";
+let color = document.querySelector("#fav_color");
+color.innerText = "White";
+let meal = document.querySelector("#fav_meal");
+meal.innerText = "Salat";
