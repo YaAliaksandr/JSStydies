01bb12741658217c2a30a84a1dc0e1013f498bb2
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-16 14:37:34 +0200

    05_Dzien_3 01_Wiecej_o_elementach 01_Zadanie_1

---
+++ 05_Dzien_3/01_Wiecej_o_elementach/01_Zadanie_1/js/app.js
@@ -0,0 +1,18 @@
+let elemLi = document.querySelector(".exercise").querySelectorAll("li");
+
+let style = function (elemArr) {
+	let elemCounter = 0;
+	for (let i = 0; i < elemArr.length; i++) {
+		elemCounter++;
+		if (elemCounter % 2 === 0) {
+			elemArr[i].style.backgroundColor = "#9e9e9e";
+		}
+		if (elemCounter === 5) {
+			elemArr[i].classList.add('big');
+		}
+		if (elemCounter % 3 === 0) {
+			elemArr[i].style.textDecoration = "underline";
+		}
+	}
+}
+style(elemLi);
