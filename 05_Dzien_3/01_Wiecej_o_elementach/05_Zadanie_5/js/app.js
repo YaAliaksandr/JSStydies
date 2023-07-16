5145900d3b8c4fbc35fabe1ed1300d1863cbba1b
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-16 17:00:42 +0200

    05_Dzien_3 01_Wiecej_o_elementach 05_Zadanie_5

--- 05_Dzien_3/01_Wiecej_o_elementach/05_Zadanie_5/js/app.js
+++ 05_Dzien_3/01_Wiecej_o_elementach/05_Zadanie_5/js/app.js
@@ -1,3 +1,15 @@
 /*
   Poniżej napisz kod rozwiązujący zadania
  */
+let exerciseLi = document.querySelector(".exercise").querySelectorAll("li");
+
+let funcForExerciseLi = function (elemArr) {
+	let counter = 0;
+	elemArr.forEach((item) => {
+		counter++;
+		item.setAttribute("data-id", counter)
+
+	});
+}
+
+funcForExerciseLi(exerciseLi);
