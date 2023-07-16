c458cb5b77408ea3194afdefacb4d59ea14af54a
Yakubouski Aliaksandr <yaaliaksandr86@gmail.com>
2023-07-16 16:50:31 +0200

    05_Dzien_3 01_Wiecej_o_elementach 04_Zadanie_4

--- 05_Dzien_3/01_Wiecej_o_elementach/04_Zadanie_4/js/app.js
+++ 05_Dzien_3/01_Wiecej_o_elementach/04_Zadanie_4/js/app.js
@@ -1,3 +1,15 @@
 /*
   Poniżej napisz kod rozwiązujący zadania
  */
+
+let excerciseUl = document.querySelector(".exercise").querySelector("ul");
+excerciseUl.classList.add("menu");
+let liExcerciseUl = excerciseUl.querySelectorAll('li');
+console.log(liExcerciseUl);
+liExcerciseUl.forEach(el => {
+	el.classList.add('menuElement');
+	let arr = el.className.split(" ");
+	if (arr.includes('error')) {
+		el.classList.remove("error");
+	}
+});
